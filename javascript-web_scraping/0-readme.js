9143cef906fc7faa43c2b095d871e929a264aa90
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-09-22 16:51:22 +0200

    fin de ligne LF

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
+// Get the file path from the command line argument
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf8", (error, data) => {
-  if (error) {
-    console.error(error);
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
   } else {
+    // Print the content of the file
     console.log(data);
   }
 });
--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+// Get the file path and string to write from command line arguments
 const filePath = process.argv[2];
-const content = process.argv[3];
+const contentToWrite = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (error) => {
-  if (error) {
-    console.error(error);
+// Write the content to the file in utf-8
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
   }
 });
